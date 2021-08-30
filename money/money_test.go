e76612e746280dafb66f5ce28ab0de4cdfbea66e
Marijn Huizendveld <marijn@huizendveld.com>
2021-08-30 10:43:42 +0200

    :mag: Identify test scenarios.

--- money/money_test.go
+++ money/money_test.go
@@ -12,7 +12,19 @@ func Test_Fails(t *testing.T) {
 
 // This test is only here to verify that false test results get detected properly in GitHub Actions
 func Test_Money_Exposes_Currency_IsoCode_for_Euro(t *testing.T) {
-	if (money.Euro != "EUR") {
+	if money.Euro != "EUR" {
 		t.Fatalf("Currency IsoCode of Euro not exposed, expected = 'EUR', actual = '%s'", money.Euro)
 	}
+}
+
+func Test_Money_Equals_detects_equal_values(t *testing.T) {
+	t.Skip("Todo")
+}
+
+func Test_Money_Equals_detects_currency_differences(t *testing.T) {
+	t.Skip("Todo")
+}
+
+func Test_Money_Equals_detects_amount_differences(t *testing.T) {
+	t.Skip("Todo")
 }
