2fb4257dfef074a5b6d3f955c8666559c45348ae
Marijn Huizendveld <marijn@huizendveld.com>
2021-08-30 10:56:00 +0200

    :mag: `Money.Equals`.

--- money/money_test.go
+++ money/money_test.go
@@ -1,19 +1,35 @@
 package money_test
 
 import (
+	"carries-cars.com/money"
 	"testing"
 )
 
 func Test_Money_Equals_detects_equal_values(t *testing.T) {
-	t.Skip("Todo")
+	actual := money.EUR(99).Equals(money.EUR(99))
+	expected := true
+
+	if actual != expected {
+		t.Fatalf("EUR(99).Equals(EUR(99)) want = %b, have = %b", expected, actual)
+	}
 }
 
 func Test_Money_Equals_detects_currency_differences(t *testing.T) {
-	t.Skip("Todo")
+	actual := money.EUR(10).Equals(money.USD(10))
+	expected := false
+
+	if actual != expected {
+		t.Fatalf("EUR(10).Equals(USD(10)) want = %b, have = %b", expected, actual)
+	}
 }
 
 func Test_Money_Equals_detects_amount_differences(t *testing.T) {
-	t.Skip("Todo")
+	actual := money.EUR(1).Equals(money.EUR(2))
+	expected := false
+
+	if actual != expected {
+		t.Fatalf("EUR(1).Equals(EUR(2)) want = %b, have = %b", expected, actual)
+	}
 }
 
 func Test_Money_Amount_exposes_value(t *testing.T) {
