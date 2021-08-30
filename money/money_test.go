ddd59603b16fa41b2de6610b26adc7e1eacefc0b
Marijn Huizendveld <marijn@huizendveld.com>
2021-08-30 11:15:59 +0200

    :mag: `Money.Multiply`

--- money/money_test.go
+++ money/money_test.go
@@ -32,6 +32,33 @@ func Test_Money_Equals_detects_amount_differences(t *testing.T) {
 	}
 }
 
+func Test_Money_Multiply_multiplies(t *testing.T) {
+	actual := money.EUR(200).Multiply(2.00)
+	expected := money.EUR(400)
+
+	if actual != expected {
+		t.Fatalf("EUR(200).Multiply(2.00) want = EUR(%v), have = EUR(%v)", expected.Amount(), actual.Amount())
+	}
+}
+
+func Test_Money_Multiply_rounds_upward_correctly(t *testing.T) {
+	actual := money.EUR(100).Multiply(1.999)
+	expected := money.EUR(200)
+
+	if actual != expected {
+		t.Fatalf("EUR(100).Multiply(1.999) want = EUR(%v), have = EUR(%v)", expected.Amount(), actual.Amount())
+	}
+}
+
+func Test_Money_Multiply_rounds_downward_correctly(t *testing.T) {
+	actual := money.EUR(100).Multiply(1.994)
+	expected := money.EUR(199)
+
+	if actual != expected {
+		t.Fatalf("EUR(100).Multiply(1.994) want = EUR(%v), have = EUR(%v)", expected.Amount(), actual.Amount())
+	}
+}
+
 func Test_Money_Amount_exposes_value(t *testing.T) {
 	t.Skip("Todo")
 }
