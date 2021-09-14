c2445056ac58d5108d1d9f2b1a7b46c7d76a833e
Marijn Huizendveld <marijn@huizendveld.com>
2021-09-14 12:15:56 +0200

    :blue_book: Rename implementation.

--- money/money.go
+++ money/money.go
@@ -11,48 +11,48 @@ type PublicMoney interface {
 	Equals(other PublicMoney) bool
 }
 
-type Money struct {
+type verifiedMoney struct {
 	// amount is denoted in the lowest denominator of the corresponding currency.
 	// E.g. amount is in whole cents for the Euro or UnitedStatesDollar
 	amount          int
 	currencyIsoCode CurrencyIsoCode
 }
 
-// EUR acts as a named constructor function to create Money for the Euro currency.
+// EUR acts as a named constructor function to create verifiedMoney for the Euro currency.
 // Provide the amount in cents.
 func EUR(amount int) PublicMoney {
-	return Money{
+	return verifiedMoney{
 		amount:          amount,
 		currencyIsoCode: Euro,
 	}
 }
 
-// USD acts as a named constructor function to create Money for the UnitedStatesDollar currency.
+// USD acts as a named constructor function to create verifiedMoney for the UnitedStatesDollar currency.
 // Provide the amount in cents.
 func USD(amount int) PublicMoney {
-	return Money{
+	return verifiedMoney{
 		amount:          amount,
 		currencyIsoCode: UnitedStatesDollar,
 	}
 }
 
-func (money Money) Equals(other PublicMoney) bool {
+func (money verifiedMoney) Equals(other PublicMoney) bool {
 	return money.amount == other.Amount() && money.currencyIsoCode == other.CurrencyIsoCode()
 }
 
-func (money Money) CurrencyIsoCode() CurrencyIsoCode {
+func (money verifiedMoney) CurrencyIsoCode() CurrencyIsoCode {
 	return money.currencyIsoCode
 }
 
-func (money Money) Amount() int {
+func (money verifiedMoney) Amount() int {
 	return money.amount
 }
 
-func (money Money) MultiplyAndRound(multiplier float64) PublicMoney {
+func (money verifiedMoney) MultiplyAndRound(multiplier float64) PublicMoney {
 	multipliedAmount := float64(money.amount) * multiplier
 	multipliedAmountRounded := int(math.Round(multipliedAmount))
 
-	return Money{
+	return verifiedMoney{
 		amount:          multipliedAmountRounded,
 		currencyIsoCode: money.currencyIsoCode,
 	}
